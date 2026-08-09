92cdc8c27f3c156142d60dbb852a95b3542125af
Sanskar-211 <agarwalsanskar211@gmail.com>
2026-08-09 18:50:41 +0530

    Update config to support multiple columns for perturbation

--- Peturbation_Engine/config.py
+++ Peturbation_Engine/config.py
@@ -1,76 +1,47 @@
 DATASET_FILE = "sample_data.csv"
 
-COLUMN_TO_PERTURB = "sensor_1"
+COLUMNS_TO_PERTURB = ["sensor_1", "sensor_2", "sensor_3"]
 
 OUTPUT_FILE = "perturbed_dataset.csv"
 
 RANDOM_SEED = 42
 
 POINT_SPIKE = {
-
-    "enabled": True,
-
-    "count": 20,
-
-    "severity": 4
-
+	"enabled": True,
+	"count": 20,
+	"severity": 4
 }
 
-
 GAUSSIAN_NOISE = {
-
-    "enabled": True,
-
-    "start": 400,
-
-    "length": 150,
-
-    "severity": 1.5
-
+	"enabled": True,
+	"start": 400,
+	"length": 150,
+	"severity": 1.5
 }
 
 BIAS = {
-
-    "enabled": True,
-
-    "start": 700,
-
-    "length": 120,
-
-    "offset": 3
-
+	"enabled": True,
+	"start": 700,
+	"length": 120,
+	"offset": 3
 }
 
 DRIFT = {
-
-    "enabled": True,
-
-    "start": 1000,
-
-    "length": 200,
-
-    "slope": 0.03
-
+	"enabled": True,
+	"start": 1000,
+	"length": 200,
+	"slope": 0.03
 }
 
 FLATLINE = {
-
-    "enabled": True,
-
-    "start": 1400,
-
-    "length": 100
-
+	"enabled": True,
+	"start": 1400,
+	"length": 100
 }
 
 DROPOUT = {
-
-    "enabled": True,
-
-    "start": 1700,
-
-    "length": 60,
-
-    "value": 0
-
+	"enabled": True,
+	"start": 1700,
+	"length": 60,
+	"value": 0
 }
